5c600691695e1a468e86d2e87ce569b2c70c74f7
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-05 15:13:54 -0300

    test: added CreateHoldingCommandTests

--- Holding.Company.Domain/Company/UseCases/Commands/Validations/CreateHoldingValidation.cs
+++ Holding.Company.Domain/Company/UseCases/Commands/Validations/CreateHoldingValidation.cs
@@ -8,7 +8,7 @@ public class CreateHoldingValidation : Contract<CreateHoldingCommand>
     {
         Requires()
             .IsNotNullOrEmpty(command.Name, "Name", "Name is required")
-            .IsLowerThan(command.Name.Length, 3, "Name", "Name must be at least 3 characters")
-            .IsGreaterThan(command.Name.Length, 80, "Name", "Name must be at most 80 characters");
+            .IsGreaterOrEqualsThan(command.Name.Length, 3, "Name", "Name must be at least 3 characters")
+            .IsLowerOrEqualsThan(command.Name.Length, 80, "Name", "Name must be at most 80 characters");
     } 
 }
---
+++ Holding.Tests/CommandTests/Company/CreateHoldingCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using Holding.Company.Domain.Company.UseCases.Commands;
+
+namespace Holding.Tests.CommandTests.Company;
+
+[TestClass]
+public class CreateHoldingCommandTests
+{
+   private static CreateHoldingCommand Sut(string name = "Holding", string? description = "Holding Description")
+       => new(name, description);
+   
+   [TestMethod]
+   public void ShoudReturnInvalidWhenCommandIsInvalid()
+   {
+       var command = Sut("");
+       Assert.AreEqual(command.IsValid, false);
+   }
+   
+   [TestMethod]
+   public void ShoudReturnValidWhenCommandIsValid()
+   {
+       var command = Sut();
+       Assert.AreEqual(command.IsValid, true);
+   }
+}
