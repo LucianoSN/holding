469a207ded9ae50e7e4ec66d247b90b485513958
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-05 00:50:19 -0300

    refactor: added Holding property

--- Holding.Company.Domain/Company/Entities/Company.cs
+++ Holding.Company.Domain/Company/Entities/Company.cs
@@ -7,13 +7,15 @@ public class Company(Guid holdingId, string name, Address address, Contact conta
     : Entity, IAggregateRoot
 {
     public Guid HoldingId { get; private set; } = holdingId;
+    public Holding? Holding { get; private set; }
+    
     public string Name { get; private set; } = name;
     public Address Address { get; private set; } = address;
     public Contact Contact { get; private set; } = contact;
-    
+
     public bool IsAccessible() => Active;
     public void ChangeName(string name) => Name = name;
     public void ChangeAddress(Address address) => Address = address;
     public void ChangeContact(Contact contact) => Contact = contact;
-    public void ChangeHolding(Guid holdingId) => HoldingId = holdingId;
+    public void ChangeHolding(Holding holding) => Holding = holding;
 }
