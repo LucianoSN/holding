1987a5d61f8d422519f8fee48d3aa0835a5215af
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-05 14:28:53 -0300

    feature: add database and repositories

---
+++ Holding.Data/Contexts/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Holding.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Holding.Data.Contexts;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options), IUnitOfWork
+{
+   public DbSet<Company.Domain.Company.Entities.Holding> Holdings { get; set; }
+   
+   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+   {
+      optionsBuilder.UseInMemoryDatabase(databaseName: "FakeDatabase");
+   }
+
+   public async Task<bool> Commit()
+   {
+      return await base.SaveChangesAsync() > 0;
+   }
+}
---
+++ Holding.Data/Repositories/CompanyRepository.cs
@@ -0,0 +1,90 @@
+﻿using Holding.Company.Domain.Company.Queries;
+using Holding.Core.Data;
+using Holding.Data.Contexts;
+
+namespace Holding.Data.Repositories;
+
+public class CompanyRepository(DataContext context) : ICompanyRepository
+{
+    public void Dispose()
+    {
+        context?.Dispose();
+    }
+
+    public IUnitOfWork UnitOfWork => context;
+    
+    public Task Create(Company.Domain.Company.Entities.Company company)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(Company.Domain.Company.Entities.Company company)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Company.Domain.Company.Entities.Company>? GetCompanyById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Company.Domain.Company.Entities.Company>? GetCompanyByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Company.Domain.Company.Entities.Company>> GetCompanyByHoldingId(Guid holdingId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Company.Domain.Company.Entities.Company>> GetAllCompanies()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Company.Domain.Company.Entities.Company>> GetAllCompaniesActivated()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Company.Domain.Company.Entities.Company>> GetAllCompaniesDeactivated()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Create(Company.Domain.Company.Entities.Holding holding)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Update(Company.Domain.Company.Entities.Holding holding)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Company.Domain.Company.Entities.Holding>? GetHoldingById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Company.Domain.Company.Entities.Holding>? GetHoldingByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Company.Domain.Company.Entities.Holding>> GetAllHolding()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Company.Domain.Company.Entities.Holding>> GetAllHoldingActivated()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Company.Domain.Company.Entities.Holding>> GetAllHoldingDeactivated()
+    {
+        throw new NotImplementedException();
+    }
+}
