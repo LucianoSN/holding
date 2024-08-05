e8cca77e92bffa1a28b3c2465e687fd76ab11f8d
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-04 22:03:23 -0300

    feat: added GlobalGroup entity

---
+++ Holding.Company.Domain/GlobalGroup/Entities/GlobalGroup.cs
@@ -0,0 +1,14 @@
+﻿using Holding.Core.DomainObjects;
+
+namespace Holding.Company.Domain.GlobalGroup.Entities;
+
+public class GlobalGroup(string name, string? description = null) : Entity
+{
+    public string Name { get; private set; } = name;
+    public string? Description { get; private set; } = description;
+    
+    public void ChangeName(string name) => Name = name;
+    public void ChangeDescription(string? description) => Description = description;
+    
+    public bool IsAccessible() => Active;
+}
---
+++ Holding.Core/DomainObjects/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace Holding.Core.DomainObjects;
+
+public abstract class Entity
+{
+    public Guid Id { get; private set; }
+    public bool Active { get; private set; } = true;
+
+    public Entity() => Id = new();
+    
+    public void Activate() => Active = true;
+    public void Deactivate() => Active = false;
+    public void ToggleActive() => Active = !Active;
+    public void ChangeActive(bool active) => Active = active;
+}
