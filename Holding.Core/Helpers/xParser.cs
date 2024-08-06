b5625fa0b484a2de7161ba724a4ea6a472b9f8a4
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-06 11:21:13 -0300

    feat: added ParserExtension

---
+++ Holding.Core/Helpers/xParser.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Holding.Core.Helpers;
+
+public static class xParser
+{
+    public static bool IsGuid(this string stringValue)
+    {
+        var guidPattern = @"[a-fA-F0-9]{8}(\-[a-fA-F0-9]{4}){3}\-[a-fA-F0-9]{12}";
+        
+        if (string.IsNullOrEmpty(stringValue)) return false;
+        
+        var guidRegEx = new Regex(guidPattern);
+        return guidRegEx.IsMatch(stringValue);
+    }
+}
