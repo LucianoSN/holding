b36c9344a1cd48028a856d87748ebe633416ec53
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-06 12:35:20 -0300

    feat: added FindHoldingByIdCommand

---
+++ Holding.Company.Domain/Company/UseCases/Commands/FindHoldingByIdCommand.cs
@@ -0,0 +1,19 @@
+﻿using Flunt.Notifications;
+using Holding.Core.DomainObjects.Results;
+using Holding.Core.Helpers;
+using MediatR;
+
+namespace Holding.Company.Domain.Company.UseCases.Commands;
+
+public class FindHoldingByIdCommand : Notifiable<Notification>, IRequest<GenericCommandResult>
+{
+    public FindHoldingByIdCommand(string id)
+    {
+        Id = Parser.ToGuid(id); 
+        
+        if (!Parser.IsGuid(id))
+            AddNotification("Id", "Id is invalid");
+    }
+    
+    public Guid Id { get; private set; }
+}
