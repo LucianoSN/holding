e94194c09009214a97fd292c666f689c5460e850
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-07 11:14:03 -0300

    test: CreateCommandTests

--- Holding.Tests/Domain/Company/CreateCompanyCommandTests.cs
+++ Holding.Tests/Domain/Company/CreateCompanyCommandTests.cs
@@ -1,7 +1,48 @@
-﻿namespace Holding.Tests.Domain.Company;
+﻿using Holding.Company.Domain.Company.UseCases.Commands;
+
+namespace Holding.Tests.Domain.Company;
 
 [TestClass]
 public class CreateCompanyCommandTests
 {
+    private static CreateCompanyCommand Sut(
+        string holdingId,
+        string name = "CompanyName",
+        string addressCountry = "CountryName",
+        string addressPostalCode = "08000000",
+        string addressState = "AddressStateName",
+        string addressStreet = "AddressStreetName",
+        string contactFullName = "ContatctFullName",
+        string contactEmail = "valid@email.com",
+        string contactPhone = "123456789"
+    )
+    {
+        var command = new CreateCompanyCommand(
+            holdingId,
+            name,
+            addressCountry,
+            addressPostalCode,
+            addressState,
+            addressStreet,
+            contactFullName,
+            contactEmail,
+            contactPhone
+        );
+
+        return command;
+    }
+
+    [TestMethod]
+    public void ShoudReturnInvalidWhenCommandIsInvalid()
+    {
+        var command = Sut("");
+        Assert.AreEqual(command.IsValid, false);
+    }
     
+   [TestMethod]
+   public void ShoudReturnValidWhenCommandIsValid()
+   {
+       var command = Sut(Guid.NewGuid().ToString());
+       Assert.AreEqual(command.IsValid, true);
+   }
 }
