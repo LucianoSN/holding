12b8405480ccfe556034b5622633da4aa3dacfa3
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-06 23:46:30 -0300

    refactor: change Company primary constructor

--- Holding.Company.Domain/Company/Entities/Company.cs
+++ Holding.Company.Domain/Company/Entities/Company.cs
@@ -3,19 +3,26 @@ using Holding.Core.ValueObjects;
 
 namespace Holding.Company.Domain.Company.Entities;
 
-public class Company(Guid holdingId, string name, Address address, Contact contact)
-    : Entity, IAggregateRoot
+public class Company : Entity, IAggregateRoot
 {
-    public Guid HoldingId { get; private set; } = holdingId;
+    public Company(Guid holdingId, string name, Address address, Contact contact)
+    {
+       HoldingId = holdingId;
+       Name = name;
+       // Address = address;
+       // Contact = contact;
+    }
+    
+    public Guid HoldingId { get; private set; }
     public Holding? Holding { get; private set; }
     
-    public string Name { get; private set; } = name;
-    public Address Address { get; private set; } = address;
-    public Contact Contact { get; private set; } = contact;
+    public string Name { get; private set; }
+    // public Address Address { get; private set; }
+    // public Contact Contact { get; private set; }
 
     public bool IsAccessible() => Active;
     public void ChangeName(string name) => Name = name;
-    public void ChangeAddress(Address address) => Address = address;
-    public void ChangeContact(Contact contact) => Contact = contact;
-    public void ChangeHolding(Holding holding) => Holding = holding;
+    // public void ChangeAddress(Address address) => Address = address;
+    // public void ChangeContact(Contact contact) => Contact = contact;
+    // public void ChangeHolding(Holding holding) => Holding = holding;
 }
