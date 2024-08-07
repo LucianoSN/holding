75e9f513c055ac44433f295b0733c84cc3c5a353
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-06 23:56:50 -0300

    chore: added OwnsOne to configure DDD implements

--- Holding.Company.Domain/Company/Entities/Company.cs
+++ Holding.Company.Domain/Company/Entities/Company.cs
@@ -5,24 +5,26 @@ namespace Holding.Company.Domain.Company.Entities;
 
 public class Company : Entity, IAggregateRoot
 {
+    protected Company() { }
+    
     public Company(Guid holdingId, string name, Address address, Contact contact)
     {
        HoldingId = holdingId;
        Name = name;
-       // Address = address;
-       // Contact = contact;
+       Address = address;
+       Contact = contact;
     }
     
     public Guid HoldingId { get; private set; }
     public Holding? Holding { get; private set; }
     
     public string Name { get; private set; }
-    // public Address Address { get; private set; }
-    // public Contact Contact { get; private set; }
+    public Address Address { get; private set; }
+    public Contact Contact { get; private set; }
 
     public bool IsAccessible() => Active;
     public void ChangeName(string name) => Name = name;
-    // public void ChangeAddress(Address address) => Address = address;
-    // public void ChangeContact(Contact contact) => Contact = contact;
-    // public void ChangeHolding(Holding holding) => Holding = holding;
+    public void ChangeAddress(Address address) => Address = address;
+    public void ChangeContact(Contact contact) => Contact = contact;
+    public void ChangeHolding(Holding holding) => Holding = holding;
 }
--- Holding.Data/Contexts/DataContext.cs
+++ Holding.Data/Contexts/DataContext.cs
@@ -6,8 +6,14 @@ namespace Holding.Data.Contexts;
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options), IUnitOfWork
 {
    public DbSet<Company.Domain.Company.Entities.Holding> Holdings { get; set; }
-   // public DbSet<Company.Domain.Company.Entities.Company> Companies { get; set; }
-   
+   public DbSet<Company.Domain.Company.Entities.Company> Companies { get; set; }
+
+   protected override void OnModelCreating(ModelBuilder modelBuilder)
+   {
+      modelBuilder.Entity<Company.Domain.Company.Entities.Company>().OwnsOne(x => x.Address);
+      modelBuilder.Entity<Company.Domain.Company.Entities.Company>().OwnsOne(x => x.Contact);
+   }
+
    public async Task<bool> Commit()
    {
       return await base.SaveChangesAsync() > 0;
