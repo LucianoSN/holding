7ff4f5ebeac9d8a8cdf991163d68207a5cbe9292
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-07 17:34:17 -0300

    feat: added FindCompanyByIdHandler

---
+++ Holding.Company.Domain/Company/UseCases/Handlers/FindCompanyByIdHandler.cs
@@ -0,0 +1,27 @@
+﻿using Holding.Company.Domain.Company.Queries;
+using Holding.Company.Domain.Company.UseCases.Commands;
+using Holding.Core.DomainObjects.Results;
+using MediatR;
+
+namespace Holding.Company.Domain.Company.UseCases.Handlers;
+
+public class FindCompanyByIdHandler(ICompanyRepository repository)
+    : IRequestHandler<FindCompanyByIdCommand, GenericCommandResult>
+{
+    public async Task<GenericCommandResult> Handle(FindCompanyByIdCommand command, 
+        CancellationToken cancellationToken)
+    {
+        // Fast Fail Validation
+        if (!command.IsValid)
+            return new GenericCommandResult(command.Notifications, false, "Ops, this company is invalid");
+        
+        // Get the holding        
+        var company = await repository.GetCompanyById(command.Id);
+        
+        // Check if the holding exists
+        if (company == null)
+            return new GenericCommandResult(null, false, "Company not found");
+        
+        return new GenericCommandResult(company, true, "Company found with success");
+    }
+}
