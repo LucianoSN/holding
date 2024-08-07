efdbb9aede8b0b32ca22bae69c306d52b900356b
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-06 23:05:40 -0300

    feat: FindHoldingById handle

---
+++ Holding.Company.Domain/Company/UseCases/Handlers/FindHoldingByIdHandler.cs
@@ -0,0 +1,26 @@
+﻿using Holding.Company.Domain.Company.Queries;
+using Holding.Company.Domain.Company.UseCases.Commands;
+using Holding.Core.DomainObjects.Results;
+using MediatR;
+
+namespace Holding.Company.Domain.Company.UseCases.Handlers;
+
+public class FindHoldingByIdHandler(ICompanyRepository repository)
+    : IRequestHandler<FindHoldingByIdCommand, GenericCommandResult>
+{
+    public async Task<GenericCommandResult> Handle(FindHoldingByIdCommand command, CancellationToken cancellationToken)
+    {
+        // Fast Fail Validation
+        if (!command.IsValid)
+            return new GenericCommandResult(command.Notifications, false, "Ops, this holding is invalid");
+        
+        // Get the holding        
+        var holding = await repository.GetHoldingById(command.Id);
+        
+        // Check if the holding exists
+        if (holding == null)
+            return new GenericCommandResult(null, false, "Holding not found");
+        
+        return new GenericCommandResult(holding, true, "Holding found with success");
+    }
+}
