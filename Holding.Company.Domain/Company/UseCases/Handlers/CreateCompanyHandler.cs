77b15c1df3319933d57035610bb10739c54a6c43
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-07 13:38:04 -0300

    feat: added CreateCompanyHandler

---
+++ Holding.Company.Domain/Company/UseCases/Handlers/CreateCompanyHandler.cs
@@ -0,0 +1,28 @@
+﻿using Holding.Company.Domain.Company.Queries;
+using Holding.Company.Domain.Company.UseCases.Commands;
+using Holding.Core.DomainObjects.Results;
+using MediatR;
+
+namespace Holding.Company.Domain.Company.UseCases.Handlers;
+
+public class CreateCompanyHandler(ICompanyRepository repository)
+    : IRequestHandler<CreateCompanyCommand, GenericCommandResult>
+{
+    public async Task<GenericCommandResult> Handle(CreateCompanyCommand command, 
+        CancellationToken cancellationToken)
+    {
+        // Fast Fail Validation
+        if (!command.IsValid)
+        {
+            return new GenericCommandResult(command.Notifications, false, "Ops, this company is invalid");
+        }
+        
+        // Generate the Company
+        var company = command.ToEntity();
+        
+        // Save in the database
+        await repository.Create(company);
+        
+        return new GenericCommandResult(company, true, "Company created");
+    }
+}
