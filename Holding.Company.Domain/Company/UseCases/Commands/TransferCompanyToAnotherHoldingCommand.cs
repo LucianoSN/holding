234015623de6dd6bd3d03a3303bdddc732043baf
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-08 14:48:10 -0300

    feat: added TransferCompanyToAnotherHoldingCommand

---
+++ Holding.Company.Domain/Company/UseCases/Commands/TransferCompanyToAnotherHoldingCommand.cs
@@ -0,0 +1,22 @@
+﻿using Flunt.Notifications;
+using Holding.Core.DomainObjects.Results;
+using Holding.Core.Helpers;
+using Holding.Core.Validations.Notifications;
+using MediatR;
+
+namespace Holding.Company.Domain.Company.UseCases.Commands;
+
+public class TransferCompanyToAnotherHoldingCommand : Notifiable<Notification>, IRequest<GenericCommandResult>
+{
+    public TransferCompanyToAnotherHoldingCommand(string companyId, string newHoldingId)
+    {
+        CompanyId = Parser.ToGuid(companyId);
+        NewHoldingId = Parser.ToGuid(newHoldingId);
+        
+        AddNotifications(new CustomNotification().IsGuid(companyId, "CompanyId", "CompanyId is invalid"));
+        AddNotifications(new CustomNotification().IsGuid(newHoldingId, "NewHoldingId", "NewHoldingId is invalid"));
+    }
+
+    public Guid CompanyId { get; private set; }
+    public Guid NewHoldingId { get; private set; }
+}
