1c9749d6ded27e567f79b7d41798aa2e1637c404
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-08 12:20:23 -0300

    feat: added ChangeCompanyHandler

---
+++ Holding.Company.Domain/Company/UseCases/Handlers/ChangeCompanyHandle.cs
@@ -0,0 +1,49 @@
+﻿using Holding.Company.Domain.Company.Queries;
+using Holding.Company.Domain.Company.UseCases.Commands;
+using Holding.Core.DomainObjects.Results;
+using Holding.Core.ValueObjects;
+using MediatR;
+
+namespace Holding.Company.Domain.Company.UseCases.Handlers;
+
+public class ChangeCompanyHandle(ICompanyRepository repository)
+    : IRequestHandler<ChangeCompanyCommand, GenericCommandResult>
+{
+    public async Task<GenericCommandResult> Handle(ChangeCompanyCommand command,
+        CancellationToken cancellationToken)
+    {
+        // Fast Fail Validation
+        if (!command.IsValid)
+            return new GenericCommandResult(command.Notifications, false, "Ops, this holding is invalid");
+
+        // Get the company        
+        var company = await repository.GetCompanyById(command.Id);
+
+        // Check if the holding exists
+        if (company == null)
+            return new GenericCommandResult(null, false, "Company not found");
+
+        // Update the company
+        company.ChangeName(command.Name);
+
+        company.ChangeAddress(
+            new Address(
+                command.AddressCountry,
+                command.AddressPostalCode,
+                command.AddressState,
+                command.AddressStreet)
+        );
+        
+        company.ChangeContact(
+            new Contact(
+                command.ContactFullName,
+                command.ContactEmail,
+                command.ContactPhone)
+        );
+        
+        // Save in the database
+        await repository.Update(company);
+        
+        return new GenericCommandResult(company, true, "Company updated with success");
+    }
+}
