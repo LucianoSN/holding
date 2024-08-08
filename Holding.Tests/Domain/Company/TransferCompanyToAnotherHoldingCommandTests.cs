d3e52e03ce6d66dff2deeafb785194293ffdd1b3
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-08 14:56:33 -0300

    test: should validate TransferCompanyToAnotherHoldingCommand

---
+++ Holding.Tests/Domain/Company/TransferCompanyToAnotherHoldingCommandTests.cs
@@ -0,0 +1,33 @@
+﻿using Holding.Company.Domain.Company.UseCases.Commands;
+
+namespace Holding.Tests.Domain.Company;
+
+[TestClass]
+public class TransferCompanyToAnotherHoldingCommandTests
+{
+    private static TransferCompanyToAnotherHoldingCommand Sut(string companyId, string holdingId)
+    {
+        return new TransferCompanyToAnotherHoldingCommand(companyId, holdingId);
+    }
+
+    [TestMethod]
+    public void ShoudReturnInvalidIfCompanyIdIsInvalid()
+    {
+        var command = Sut("", Guid.NewGuid().ToString());
+        Assert.AreEqual(command.IsValid, false);
+    }
+
+    [TestMethod]
+    public void ShoudReturnInvalidIfHoldingIdIsInvalid()
+    {
+        var command = Sut(Guid.NewGuid().ToString(), "");
+        Assert.AreEqual(command.IsValid, false);
+    }
+
+    [TestMethod]
+    public void ShoudReturnValidTransferCompanyToAnotherHoldingCommnand()
+    {
+        var command = Sut(Guid.NewGuid().ToString(), Guid.NewGuid().ToString());
+        Assert.AreEqual(command.IsValid, true);
+    }
+}
