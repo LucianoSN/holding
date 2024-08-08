0ce57b56c6dfe7b70e5d0b0882209302ac9ceaee
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-08 15:18:12 -0300

    feat: added TransferCompanyToAnotherHoldingHandler

---
+++ Holding.Company.Domain/Company/UseCases/Handlers/TransferCompanyToAnotherHoldingHandler.cs
@@ -0,0 +1,33 @@
+﻿using Holding.Company.Domain.Company.Queries;
+using Holding.Company.Domain.Company.UseCases.Commands;
+using Holding.Core.DomainObjects.Results;
+using MediatR;
+
+namespace Holding.Company.Domain.Company.UseCases.Handlers;
+
+public class TransferCompanyToAnotherHoldingHandler(ICompanyRepository repository)
+    : IRequestHandler<TransferCompanyToAnotherHoldingCommand, GenericCommandResult>
+{
+    public async Task<GenericCommandResult> Handle(TransferCompanyToAnotherHoldingCommand command, 
+        CancellationToken cancellationToken)
+    {
+        // Fast Fail Validation
+        if (!command.IsValid)
+            return new GenericCommandResult(command.Notifications, false, "Ops, this holding is invalid");
+
+        // Get the company        
+        var company = await repository.GetCompanyById(command.CompanyId);
+
+        // Check if the holding exists
+        if (company == null)
+            return new GenericCommandResult(null, false, "Company not found");
+        
+        // Update the company
+        company.ChangeHolding(command.NewHoldingId);
+        
+        // Save in the database
+        await repository.Update(company);
+        
+        return new GenericCommandResult(company, true, "Company updated with success");
+    }
+}
