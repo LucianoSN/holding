813ba85882d0421a254b58396b03a15eed9e27f5
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-08 01:00:50 -0300

    feat: added GetAllCompanies command and handler

---
+++ Holding.Company.Domain/Company/UseCases/Commands/GetAllCompaniesCommand.cs
@@ -0,0 +1,10 @@
+﻿using Flunt.Notifications;
+using Holding.Core.DomainObjects.Results;
+using MediatR;
+
+namespace Holding.Company.Domain.Company.UseCases.Commands;
+
+public class GetAllCompaniesCommand : Notifiable<Notification>, IRequest<GenericCommandResult>
+{
+    
+}
---
+++ Holding.Company.Domain/Company/UseCases/Handlers/GetAllCompaniesHandler.cs
@@ -0,0 +1,16 @@
+﻿using Holding.Company.Domain.Company.Queries;
+using Holding.Company.Domain.Company.UseCases.Commands;
+using Holding.Core.DomainObjects.Results;
+using MediatR;
+
+namespace Holding.Company.Domain.Company.UseCases.Handlers;
+
+public class GetAllCompaniesHandler(ICompanyRepository repository)
+    : IRequestHandler<GetAllCompaniesCommand, GenericCommandResult>
+{
+    public async Task<GenericCommandResult> Handle(GetAllCompaniesCommand request, CancellationToken cancellationToken)
+    {
+        var companies = await repository.GetAllHoldings();
+        return new GenericCommandResult(companies, true, "Companies retrieved with success");
+    }
+}
