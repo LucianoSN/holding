2472f38e2c1964a84a299d979a2d9b5bff13e924
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-10 17:37:59 -0300

    feat: create group command

---
+++ Holding.Company.Domain/Division/UseCases/Commands/CreateGroupCommand.cs
@@ -0,0 +1,23 @@
+﻿using Flunt.Notifications;
+using Holding.Company.Domain.Division.UseCases.Commands.Validations;
+using Holding.Core.DomainObjects.Results;
+using Holding.Core.Helpers;
+using Holding.Core.Validations.Notifications;
+using MediatR;
+
+namespace Holding.Company.Domain.Division.UseCases.Commands;
+
+public class CreateGroupCommand : Notifiable<Notification>, IRequest<GenericCommandResult>
+{
+    public CreateGroupCommand(string companyId, string name)
+    {
+        CompanyId = Parser.ToGuid(companyId);
+        Name = name;
+        
+        AddNotifications(new CreateGroupValidation(this));
+        AddNotifications(new CustomNotification().IsGuid(companyId, "CompanyId", "CompanyId is invalid"));
+    }
+
+    public Guid CompanyId { get; private set; }
+    public string Name { get; private set; }
+}
