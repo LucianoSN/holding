b68f877cf70a9bf5fb24ffd08976b4c31e860e83
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-10 17:46:51 -0300

    test: should validade CreateGroupCommand

---
+++ Holding.Tests/Domain/Division/CreateGroupCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using Holding.Company.Domain.Division.UseCases.Commands;
+
+namespace Holding.Tests.Domain.Division;
+
+[TestClass]
+public class CreateGroupCommandTests
+{
+   private static CreateGroupCommand Sut(string companyId, string name = "")
+       => new(companyId, name); 
+   
+   [TestMethod]
+   public void ShoudReturnInvalidWhenCommandIsInvalid()
+   {
+       var command = Sut("");
+       Assert.AreEqual(command.IsValid, false);
+   }
+   
+   [TestMethod]
+   public void ShoudReturnValidWhenCommandIsValid()
+   {
+       var command = Sut(Guid.NewGuid().ToString(), "AnyGroup");
+       Assert.AreEqual(command.IsValid, true);
+   }
+}
