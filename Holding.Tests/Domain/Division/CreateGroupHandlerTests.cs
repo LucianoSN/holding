dc093b061c8211b225c25cb8bcaf609c013a9589
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-10 18:08:10 -0300

    test: should return success if CreateGroupHandler is valid

--- Holding.Tests/Domain/Division/CreateGroupHandlerTests.cs
+++ Holding.Tests/Domain/Division/CreateGroupHandlerTests.cs
@@ -1,4 +1,5 @@
-﻿using Holding.Company.Domain.Division.Queries;
+﻿using Holding.Company.Domain.Division.Entities;
+using Holding.Company.Domain.Division.Queries;
 using Holding.Company.Domain.Division.UseCases.Commands;
 using Holding.Company.Domain.Division.UseCases.Handlers;
 
@@ -9,25 +10,43 @@ public class CreateGroupHandlerTests
 {
     private IGroupRepository _repository;
     private readonly CreateGroupHandler _sut;
-    private string _companyId;
+    private Guid _companyId;
 
     public CreateGroupHandlerTests()
     {
         _repository = Helper.GetRequiredService<IGroupRepository>();
         _sut = new CreateGroupHandler(_repository);
-        _companyId = Guid.NewGuid().ToString();
+        _companyId = Guid.NewGuid();
     }
     
     [TestMethod]
     public async Task ShouldCreateGroupIsInvalid()
     {
         // Arrange
-        var command = new CreateGroupCommand(_companyId, "");
+        var command = new CreateGroupCommand(_companyId.ToString(), "");
     
         // Act
         var result = await _sut.Handle(command, CancellationToken.None);
     
         // Assert
         Assert.AreEqual(result.Success, false);
+    }
+
+
+    [TestMethod]
+    public async Task ShouldCreateHoldingIsValid()
+    {
+        // Arrange
+        var command = new CreateGroupCommand(_companyId.ToString(), "Group Test");
+    
+        // Act
+        var result = await _sut.Handle(command, CancellationToken.None);
+        if (result.Success) await _repository.Transact.Commit();
+        var group = result.Data as Group;
+    
+        // Assert
+        Assert.AreEqual(result.Success, true);
+        Assert.IsNotNull(group);
+        Assert.AreEqual(group.CompanyId, _companyId);
     }
 }
