2aa928baa3ef20d51d2781a1ae78365e85381abb
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-12 19:00:44 -0300

    feat: added CreateSubGroupCommand

---
+++ Holding.Company.Domain/Division/UseCases/Commands/CreateSubGroupCommand.cs
@@ -0,0 +1,25 @@
+﻿using Flunt.Notifications;
+using Holding.Company.Domain.Division.UseCases.Commands.Validations;
+using Holding.Core.DomainObjects.Results;
+using Holding.Core.Helpers;
+using Holding.Core.Validations.Notifications;
+using MediatR;
+
+namespace Holding.Company.Domain.Division.UseCases.Commands;
+
+public class CreateSubGroupCommand : Notifiable<Notification>, IRequest<GenericCommandResult>
+{
+    public CreateSubGroupCommand(string groupId, string name)
+    {
+        GroupId = Parser.ToGuid(groupId);
+        Name = name;
+        
+        AddNotifications(new CreateSubGroupValidation(this));
+        AddNotifications(new CustomNotification().IsGuid(groupId, "GroupId", "GroupId is invalid"));
+    }
+        
+    public Guid GroupId { get; private set; }
+    public string Name { get; private set; }
+    
+    public Entities.SubGroup ToEntity() => new(GroupId, Name);
+}
---
+++ Holding.Company.Domain/Division/UseCases/Commands/Validations/CreateSubGroupValidation.cs
@@ -0,0 +1,14 @@
+﻿using Flunt.Validations;
+
+namespace Holding.Company.Domain.Division.UseCases.Commands.Validations;
+
+public class CreateSubGroupValidation : Contract<CreateSubGroupCommand>
+{
+    public CreateSubGroupValidation(CreateSubGroupCommand command)
+    {
+        Requires()
+            .IsNotNullOrEmpty(command.Name, "Name", "Name is required")
+            .IsGreaterOrEqualsThan(command.Name.Length, 3, "Name", "Name must be at least 3 characters")
+            .IsLowerOrEqualsThan(command.Name.Length, 80, "Name", "Name must be at most 80 characters");
+    } 
+}
