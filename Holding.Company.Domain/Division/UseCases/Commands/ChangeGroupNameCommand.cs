e8c30cbc17160a4f9a2d558c2d77332878f98ec7
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-12 16:30:43 -0300

    feat: change name in group

---
+++ Holding.Company.Domain/Division/UseCases/Commands/ChangeGroupNameCommand.cs
@@ -0,0 +1,23 @@
+﻿using Flunt.Notifications;
+using Holding.Company.Domain.Division.UseCases.Commands.Validations;
+using Holding.Core.DomainObjects.Results;
+using Holding.Core.Helpers;
+using Holding.Core.Validations.Notifications;
+using MediatR;
+
+namespace Holding.Company.Domain.Division.UseCases.Commands;
+
+public class ChangeGroupNameCommand : Notifiable<Notification>, IRequest<GenericCommandResult>
+{
+    public ChangeGroupNameCommand(string id, string name)
+    {
+        Id = Parser.ToGuid(id);
+        Name = name;
+        
+        AddNotifications(new ChangeGroupNameValidation(this));
+        AddNotifications(new CustomNotification().IsGuid(id, "Id", "Id is invalid"));
+    }
+
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }    
+}
