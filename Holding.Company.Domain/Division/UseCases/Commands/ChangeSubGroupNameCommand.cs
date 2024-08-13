7e0e11f58b8df0ecef6f08b51a67068970033e47
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-13 13:10:44 -0300

    feat: added ChangeSubGroupNameCommand

---
+++ Holding.Company.Domain/Division/UseCases/Commands/ChangeSubGroupNameCommand.cs
@@ -0,0 +1,26 @@
+﻿using Flunt.Notifications;
+using Holding.Company.Domain.Division.UseCases.Commands.Validations;
+using Holding.Core.DomainObjects.Results;
+using Holding.Core.Helpers;
+using Holding.Core.Validations.Notifications;
+using MediatR;
+
+namespace Holding.Company.Domain.Division.UseCases.Commands;
+
+public class ChangeSubGroupNameCommand : Notifiable<Notification>, IRequest<GenericCommandResult>
+{
+    public ChangeSubGroupNameCommand(string id, string groupId, string name)
+    {
+        Id = Parser.ToGuid(id);
+        GroupId = Parser.ToGuid(groupId);
+        Name = name;
+
+        AddNotifications(new ChangeSubGroupNameValidation(this));
+        AddNotifications(new CustomNotification().IsGuid(id, "Id", "Id is invalid"));
+        AddNotifications(new CustomNotification().IsGuid(groupId, "GroupId", "GroupId is invalid"));
+    }
+
+    public Guid Id { get; private set; }
+    public Guid GroupId { get; private set; }
+    public string Name { get; private set; }
+}
