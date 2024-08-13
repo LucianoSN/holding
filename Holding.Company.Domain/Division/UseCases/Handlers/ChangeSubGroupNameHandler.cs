4a8d929412c43d75aab0d5e43ff608ebbbe71bdc
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-13 14:10:04 -0300

    feat: adde ChangeSubGroupNameHandler

---
+++ Holding.Company.Domain/Division/UseCases/Handlers/ChangeSubGroupNameHandler.cs
@@ -0,0 +1,41 @@
+﻿using Holding.Company.Domain.Division.Queries;
+using Holding.Company.Domain.Division.UseCases.Commands;
+using Holding.Core.DomainObjects.Results;
+using MediatR;
+
+namespace Holding.Company.Domain.Division.UseCases.Handlers;
+
+public class ChangeSubGroupNameHandler(IGroupRepository repository)
+    : IRequestHandler<ChangeSubGroupNameCommand, GenericCommandResult>
+{
+    public async Task<GenericCommandResult> Handle(ChangeSubGroupNameCommand command, 
+        CancellationToken cancellationToken)
+    {
+        // Fast Fail Validation
+        if (!command.IsValid)
+            return new GenericCommandResult(command.Notifications, false, "Ops, this SubGroup is invalid");
+
+        // Get the group        
+        var group = await repository.GetGroupByIdWithSubGroupsTracking(command.GroupId);
+
+        // Check if the group exists
+        if (group == null)
+            return new GenericCommandResult(null, false, "Group not found");
+
+        // Change the subgroup name
+        var changeSubGroupName = group.ChangeSubGroupName(command.Id, command.Name);
+
+        // Check if the subgroup already exists
+        if (!changeSubGroupName)
+            return new GenericCommandResult(command.Notifications, false, "SubGroup already exists");
+            
+        await repository.Update(group);
+        await repository.Transact.Commit();
+
+        return new GenericCommandResult(
+            group,
+            changeSubGroupName,
+            "SubGroup name changed with success"
+        );
+    }
+}
