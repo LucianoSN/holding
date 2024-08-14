ee79b1bcd6ff8e4ee5114f75db4410c7bee185b6
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-14 13:27:46 -0300

    refactor: remove unused code

--- Holding.Company.Domain/Company/UseCases/Handlers/ChangeCompanyHandler.cs
+++ Holding.Company.Domain/Company/UseCases/Handlers/ChangeCompanyHandler.cs
@@ -14,7 +14,7 @@ public class ChangeCompanyHandler(ICompanyRepository repository)
     {
         // Fast Fail Validation
         if (!command.IsValid)
-            return new GenericCommandResult(command.Notifications, false, "Ops, this holding is invalid");
+            return new GenericCommandResult(command.Notifications, false, "Ops, this company is invalid");
 
         // Get the company        
         var company = await repository.GetCompanyById(command.Id);
