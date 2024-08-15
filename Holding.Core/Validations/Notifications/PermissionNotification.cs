b118bfb5f02d4d40b76647cae3e5f686be362587
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-15 16:35:13 -0300

    refactor: set default value to role parameter

--- Holding.Core/Validations/Notifications/PermissionNotification.cs
+++ Holding.Core/Validations/Notifications/PermissionNotification.cs
@@ -6,7 +6,7 @@ namespace Holding.Core.Validations.Notifications;
 
 public partial class CustomNotification
 {
-    public Notifiable<Notification> HasPermission<T>(string role) where T: BasePermission
+    public Notifiable<Notification> HasPermission<T>(string role = "") where T: BasePermission
     {
         var permission =  (T)Activator.CreateInstance(typeof(T), new object[] { role.ToRole() })!;
         
