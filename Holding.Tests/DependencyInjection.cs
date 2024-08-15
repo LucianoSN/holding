ccd71990bff001727e929e31b10e2f5c4c09ca2f
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-15 14:01:37 -0300

    chore: remove unused packages

--- Holding.Tests/DependencyInjection.cs
+++ Holding.Tests/DependencyInjection.cs
@@ -12,9 +12,6 @@ public static class DependencyInjection
     {
         var services = new ServiceCollection();
         
-        // services.AddDbContext<DataContext>(options =>
-        //     options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
-        
         services.AddDbContext<DataContext>(options =>
             options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
 
