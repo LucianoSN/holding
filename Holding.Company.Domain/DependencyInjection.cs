17e3003636ffdc2a831945e186f20f1dae41e952
Luciano Nery <luciano@primeiraescolha.com.br>
2024-08-15 15:38:15 -0300

    chore: change scoped to transient in mediatr

--- Holding.Company.Domain/DependencyInjection.cs
+++ Holding.Company.Domain/DependencyInjection.cs
@@ -9,7 +9,7 @@ public static class DependencyInjection
    public static IServiceCollection AddDomainCompany(this IServiceCollection services)
    {
        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-       services.AddScoped<IMediator, Mediator>();
+       services.AddTransient<IMediator, Mediator>();
        
        return services;
    }
